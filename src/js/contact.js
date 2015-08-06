e58bf9a65990aa8d4c94dbade6e76fe14beac3a0
Osvaldo Fernandez <osvaldo.fernandez@wolox.com.ar>
2015-08-06 16:43:23 -0300

    changed mandrill for sendgrid

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,27 +3,22 @@ $('#send').click(function() {
   var email = document.getElementById('email').value;
   var message = document.getElementById('message').value;
   var name = document.getElementById('name').value;
+
   $.ajax({
     type: 'POST',
-    url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+    url: 'https://api.sendgrid.com/api/mail.send.json',
     data: {
-      'key': window.key,
-      'message': {
-        'from_email': email,
-        'from_name': name,
-        'to': [
-          {
-            'email': window.soleadoEmail,
-            'name': window.soleadoName,
-            'type': 'to'
-          }
-        ],
-      'autotext': 'true',
-      'subject': window.subject,
-      'html': message
+      api_user:window.user,
+      api_key: window.key,
+      to: window.soleadoEmail,
+      toname: window.soleadoName,
+      subject: window.subject,
+      text: message,
+      from: email,
+      fromname: name
     }
-  }
- }).done(function() {
-    $('#contact-caption').text('YOUR INFO HAS BEEN SENT');
-  });
+  })
+  $('#contact-caption').text('YOUR INFO HAS BEEN SENT');
 });
+
+
